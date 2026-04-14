f7bc47d0e59edb5b4bda82cdde9557af14673a2f
Christian <christianjfr@gmail.com>
2026-04-14 14:41:12 -0500

    chore: refactor toggle button

--- src/scripts/toggle.js
+++ src/scripts/toggle.js
@@ -4,22 +4,25 @@ const $body = document.querySelector('body');
 const isDarkMode = window.matchMedia('(prefers-color-scheme: dark)').matches;
 
 document.addEventListener('DOMContentLoaded', () => {
-	if (isDarkMode) {
-		$body.classList.add('dark');
-	} else {
-		$body.classList.remove('dark');
-		$toggle.classList.toggle('active');
-	}
+  changeTheme()
+	// if (isDarkMode) {
+	// 	$body.classList.add('dark');
+	// } else {
+	// 	$body.classList.remove('dark');
+	// 	$toggle.classList.toggle('active');
+	// }
 });
 
 $toggle.addEventListener('click', changeTheme);
 
 function changeTheme() {
-	if ($body.classList.contains('dark')) {
-		$body.classList.remove('dark');
-		$toggle.classList.toggle('active');
-	} else {
-		$body.classList.add('dark');
-		$toggle.classList.toggle('active');
-	}
+  $body.classList.toggle('dark');
+  $toggle.classList.toggle('active');
+	// if ($body.classList.contains('dark')) {
+	// 	$body.classList.remove('dark');
+	// 	$toggle.classList.toggle('active');
+	// } else {
+	// 	$body.classList.add('dark');
+	// 	$toggle.classList.toggle('active');
+	// }
 }
