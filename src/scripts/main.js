2e004057f5cc95b490b5a7666a3a4fb573c86fef
Christian <christianjfr@gmail.com>
2026-04-18 16:23:11 -0500

    chore: delete duplicated function

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,6 +1,5 @@
 import { data } from "./data";
 
-const inputs = document.querySelectorAll("input");
 const $currentLevel = document.querySelector("#input-current-level");
 const $currentExp = document.querySelector("#input-current-exp");
 const $targetLevel = document.querySelector("#input-target-level");
@@ -13,7 +12,7 @@ const $neededExp = document.querySelector("#input-needed-exp");
 const $craftExp = document.querySelector("#input-craft-exp");
 
 const $validOk = document.querySelector("#input-valid-ok");
-const $whyNot = document.querySelector(".why-not");
+const $statusMessage = document.querySelector(".status-message");
 
 const $total = document.querySelector("#input-total");
 const $divTotal = document.querySelector(".total");
@@ -30,6 +29,55 @@ const getTargetExp = (value) => {
   const targetExp = data.expData[value] || 0;
   $targetExp.value = targetExp;
   return targetExp;
+};
+
+const validateOk = (slot, target, level, exp) => {
+  if (
+    isNaN(slot) ||
+    isNaN(target) ||
+    isNaN(level) ||
+    isNaN(exp) ||
+    slot < 2 ||
+    slot > 7 ||
+    target < 1 ||
+    target > 100 ||
+    level < 1 ||
+    level > 99
+  ) {
+    $validOk.value = "ERROR";
+    $validOk.classList.add("input-invalid-ok");
+    $statusMessage.textContent = "Por favor, verifica todos los campos";
+  } else if (level >= target) {
+    $validOk.value = "ERROR";
+    $validOk.classList.add("input-invalid-ok");
+    $statusMessage.textContent = "El nivel de oficio objetivo debe ser mayor al actual";
+  } else if ((target > 80 && slot < 4) || (target > 60 && slot < 3) || (target > 40 && slot < 2)) {
+    $validOk.value = "ERROR";
+    $validOk.classList.add("input-invalid-ok");
+    $statusMessage.textContent = "Cantidad de Ranuras en la receta es muy bajo";
+  } else if (
+    (level < 80 && slot >= 7) ||
+    (level < 60 && slot >= 6) ||
+    (level < 40 && slot >= 5) ||
+    (level < 20 && slot >= 4) ||
+    (level < 10 && slot >= 3)
+  ) {
+    $validOk.value = "ERROR";
+    $validOk.classList.add("input-invalid-ok");
+    $statusMessage.textContent = "Tu nivel de Oficio no te permite fabricar esta receta";
+  } else if (
+    $currentExp.value < data.expData[$currentLevel.value] ||
+    $currentExp.value >= data.expData[$targetLevel.value] ||
+    $currentExp.value >= data.expData[parseInt($currentLevel.value) + 1]
+  ) {
+    $validOk.value = "ERROR";
+    $validOk.classList.add("input-invalid-ok");
+    $statusMessage.textContent = "Revisa tu experiencia o nivel de oficio";
+  } else {
+    $validOk.value = "OK";
+    $validOk.classList.remove("input-invalid-ok");
+    $statusMessage.textContent = "Valores validos para calcular";
+  }
 };
 
 const updateAll = () => {
@@ -51,79 +99,12 @@ const updateAll = () => {
   $calculateButton.disabled = $validOk.value === "ERROR";
 };
 
-$currentLevel.addEventListener("input", () => {
-  $currentExp.value = data.expData[$currentLevel.value];
-  updateAll();
-});
-
-$currentExp.addEventListener("input", updateAll);
-$targetLevel.addEventListener("input", updateAll);
-$slots.addEventListener("input", updateAll);
-$bonus.addEventListener("change", updateAll);
-
-$calculateButton.addEventListener("click", calculateTotal);
-
-inputs.forEach((input) => {
-  input.addEventListener("input", () => {
-    $divTotal.classList.add("total-hide");
-    $divTotal.classList.remove("total-show");
-  });
-});
-
-const validateOk = (slot, target, level, exp) => {
-  if (
-    isNaN(slot) ||
-    isNaN(target) ||
-    isNaN(level) ||
-    isNaN(exp) ||
-    slot < 2 ||
-    slot > 7 ||
-    target < 1 ||
-    target > 100 ||
-    level < 1 ||
-    level > 99
-  ) {
-    $validOk.value = "ERROR";
-    $validOk.classList.add("input-invalid-ok");
-    $whyNot.textContent = "Por favor, verifica todos los campos";
-  } else if (level >= target) {
-    $validOk.value = "ERROR";
-    $validOk.classList.add("input-invalid-ok");
-    $whyNot.textContent = "El nivel de oficio objetivo debe ser mayor al actual";
-  } else if ((target > 80 && slot < 4) || (target > 60 && slot < 3) || (target > 40 && slot < 2)) {
-    $validOk.value = "ERROR";
-    $validOk.classList.add("input-invalid-ok");
-    $whyNot.textContent = "Cantidad de Ranuras en la receta es muy bajo";
-  } else if (
-    (level < 80 && slot >= 7) ||
-    (level < 60 && slot >= 6) ||
-    (level < 40 && slot >= 5) ||
-    (level < 20 && slot >= 4) ||
-    (level < 10 && slot >= 3)
-  ) {
-    $validOk.value = "ERROR";
-    $validOk.classList.add("input-invalid-ok");
-    $whyNot.textContent = "Tu nivel de Oficio no te permite fabricar esta receta";
-  } else if (
-    $currentExp.value < data.expData[$currentLevel.value] ||
-    $currentExp.value >= data.expData[$targetLevel.value] ||
-    $currentExp.value >= data.expData[parseInt($currentLevel.value) + 1]
-  ) {
-    $validOk.value = "ERROR";
-    $validOk.classList.add("input-invalid-ok");
-    $whyNot.textContent = "Revisa tu experiencia o nivel de oficio";
-  } else {
-    $validOk.value = "OK";
-    $validOk.classList.remove("input-invalid-ok");
-    $whyNot.textContent = "Valores validos para calcular";
-  }
+const showTotalDiv = () => {
+  $divTotal.classList.remove("total-hide");
+  $divTotal.classList.add("total-show");
 };
 
-document.addEventListener("DOMContentLoaded", function () {
-  updateAll();
-});
-
-function calculateTotal() {
+const calculateTotal = () => {
   showTotalDiv();
   let total = $neededExp.value / $craftExp.value;
   total = Math.ceil(total);
@@ -139,9 +120,19 @@ function calculateTotal() {
   } else {
     $total.textContent = total;
   }
-}
+};
 
-function showTotalDiv() {
-  $divTotal.classList.remove("total-hide");
-  $divTotal.classList.add("total-show");
-}
+$currentLevel.addEventListener("input", () => {
+  $currentExp.value = data.expData[$currentLevel.value];
+  updateAll();
+});
+$currentExp.addEventListener("input", updateAll);
+$targetLevel.addEventListener("input", updateAll);
+$slots.addEventListener("input", updateAll);
+$bonus.addEventListener("change", updateAll);
+
+$calculateButton.addEventListener("click", calculateTotal);
+
+document.addEventListener("DOMContentLoaded", function () {
+  updateAll();
+});
